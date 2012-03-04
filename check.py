a2c7f7e0437c9ee8383a616840f50ee4aa77d510
Paul Berry <stereotype441@gmail.com>
2012-03-04 11:18:59 -0800

    Check function names

--- check.py
+++ check.py
@@ -20,6 +20,15 @@ ALLOWED_NESTINGS = (('OpenGLAPI', 'category'),
                     )
 
 
+error_found = False
+
+
+def error(desc):
+    global error_found
+    error_found = True
+    print 'Error: {0}'.format(desc)
+
+
 def short_desc(elem):
     if 'name' in elem.attrib:
         return '<{0} name="{1}">'.format(elem.tag, elem.attrib['name'])
@@ -28,24 +37,18 @@ def short_desc(elem):
 
 
 def check_nesting(elem):
-    ok = True
     for sub_elem in elem.findall('*'):
         if (elem.tag, sub_elem.tag) not in ALLOWED_NESTINGS:
-            ok = False
-            print 'Error: {0} nested inside {1}'.format(
-                short_desc(sub_elem), short_desc(elem))
-        ok = check_nesting(sub_elem) and ok
-    return ok
+            error('{0} nested inside {1}'.format(
+                    short_desc(sub_elem), short_desc(elem)))
+        check_nesting(sub_elem)
 
 
 def check_categories(api_xml):
-    ok = True
     for category_xml in api_xml.findall('.//category'):
         name = category_xml.attrib['name']
         if not VALID_CATEGORY_REGEXP.match(name):
-            ok = False
-            print 'Invalid category: {0!r}'.format(name)
-    return ok
+            error('Invalid category: {0!r}'.format(name))
 
 
 def check_enum_value(name, value):
@@ -55,39 +58,48 @@ def check_enum_value(name, value):
         value = value[:-1]
     try:
         value_int = int(value, 0)
-        return True
+        return
     except ValueError:
         pass
-    print "Don't know how to interpret value of enum {0} ({1!r})".format(
-        name, value)
-    return False
+    error("Don't know how to interpret value of enum {0} ({1!r})".format(
+            name, value))
 
 
 def check_enum_name(name):
     if name.startswith('GL_'):
-        print 'Error: invalid enum name {0!r}'.format(name)
-        return False
-    return True
+        error('invalid enum name {0!r}'.format(name))
 
 
 def check_enums(api_xml):
-    ok = True
     name_to_values_dict = collections.defaultdict(set)
     for enum_xml in api_xml.findall('.//enum'):
         name = enum_xml.attrib['name']
         value = enum_xml.attrib['value']
-        ok = check_enum_value(name, value) and ok
-        ok = check_enum_name(name) and ok
+        check_enum_value(name, value)
+        check_enum_name(name)
         name_to_values_dict[name].add(value)
-    print 'Found {0} enum declarations'.format(len(name_to_values_dict))
+    print 'Found {0} unique enum names'.format(len(name_to_values_dict))
     for name in sorted(name_to_values_dict.keys()):
         values = name_to_values_dict[name]
         if len(values) != 1:
-            ok = False
-            print 'Error: inconsistent definitions of enum {0!r}:'.format(name)
+            error_desc = 'inconsistent definitions of enum {0!r}:'.format(name)
             for value in values:
-                print '  {0}'.format(value)
-    return ok
+                error_desc += '\n  {0}'.format(value)
+            error(error_desc)
+
+
+def check_function_name(name):
+    if name.startswith('gl'):
+        error('invalid function name {0!r}'.format(name))
+
+
+def check_functions(api_xml):
+    name_to_xml_dict = collections.defaultdict(list)
+    for function_xml in api_xml.findall('.//function'):
+        name = function_xml.attrib['name']
+        check_function_name(name)
+        name_to_xml_dict[name].append(function_xml)
+    print 'Found {0} unique function names'.format(len(name_to_xml_dict))
 
 
 if __name__ == '__main__':
@@ -96,9 +108,9 @@ if __name__ == '__main__':
     basename = os.path.basename(file_to_parse)
     root = xml.etree.ElementTree.parse(basename).getroot()
     xml.etree.ElementInclude.include(root)
-    ok = True
-    ok = check_nesting(root) and ok
-    ok = check_categories(root) and ok
-    ok = check_enums(root) and ok
-    if not ok:
+    check_nesting(root)
+    check_categories(root)
+    check_enums(root)
+    check_functions(root)
+    if error_found:
         exit(1)
