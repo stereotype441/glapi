a834f9a61f570d531094cbbab3469aec6e21459b
Paul Berry <stereotype441@gmail.com>
2012-03-04 11:50:23 -0800

    Check types

--- check.py
+++ check.py
@@ -18,6 +18,8 @@ ALLOWED_NESTINGS = (('OpenGLAPI', 'category'),
                     ('function', 'return'),
                     ('enum', 'size'),
                     )
+BASE_TYPES = ()
+RECOGNIZED_STRUCTS = ('_cl_context', '_cl_event')
 
 
 error_found = False
@@ -57,8 +59,9 @@ def check_types(api_xml):
         name = type_xml.attrib['name']
         if name.startswith('GL'):
             error('Invalid type name {0!r}'.format(name))
-        types.add(name)
+        types.add('GL' + name)
     print 'Found {0} unique type names'.format(len(types))
+    return types
 
 
 def check_enum_value(name, value):
@@ -103,12 +106,50 @@ def check_function_name(name):
         error('invalid function name {0!r}'.format(name))
 
 
-def check_functions(api_xml):
+def check_type_usage(function, typ, gl_types):
+    type_components = typ.replace('*', ' * ').split(' ')
+    type_components = [x for x in type_components if x != '']
+    if len(type_components) == 0:
+        error('{0} uses empty type'.format(short_desc(function)))
+    i = 0
+    while i < len(type_components):
+        component = type_components[i]
+        if (component in ('const', '*') or
+            component in gl_types or
+            component in BASE_TYPES):
+            pass
+        elif component == 'struct':
+            i += 1
+            if i == len(type_components):
+                error('{0} uses invalid type {1!r}'.format(
+                        short_desc(function), typ))
+                break
+            else:
+                if type_components[i] not in RECOGNIZED_STRUCTS:
+                    error('{0} uses unrecognized struct {1}'.format(
+                            short_desc(function), type_components[i]))
+        else:
+            error('{0} uses unrecognized type {1!r}'.format(
+                    short_desc(function), component))
+        i += 1
+
+
+def check_functions(api_xml, gl_types):
     name_to_xml_dict = collections.defaultdict(list)
     for function_xml in api_xml.findall('.//function'):
         name = function_xml.attrib['name']
         check_function_name(name)
         name_to_xml_dict[name].append(function_xml)
+        for param_xml in function_xml.findall('param'):
+            check_type_usage(function_xml, param_xml.attrib['type'],
+                             gl_types)
+        return_xmls = function_xml.findall('return')
+        if len(return_xmls) > 1:
+            error('{0} declares multiple returns'.format(
+                    short_desc(function_xml)))
+        for return_xml in return_xmls:
+            check_type_usage(function_xml, return_xml.attrib['type'],
+                             gl_types)
     print 'Found {0} unique function names'.format(len(name_to_xml_dict))
 
 
@@ -120,8 +161,8 @@ if __name__ == '__main__':
     xml.etree.ElementInclude.include(root)
     check_nesting(root)
     check_categories(root)
-    check_types(root)
+    gl_types = check_types(root)
     check_enums(root)
-    check_functions(root)
+    check_functions(root, gl_types)
     if error_found:
         exit(1)
